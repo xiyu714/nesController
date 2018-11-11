2605b2e2faba2a4aad9c9f90cf362dfa00fbaf95
xiyu <daixiyu714@outlook.com>
2018-11-12 00:22:02 +0800

    还需要，添加处理流量

--- main/main.go
+++ main/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strconv"
 	"strings"
+	"time"
 )
 
 const netDev = "/proc/net/dev"
@@ -24,19 +26,36 @@ type NetDevice struct {
 }
 
 type NetInfo struct {
-	bytes      string
-	packets    string
-	errs       string
-	drop       string
-	fifo       string
-	frame      string
-	compressed string
-	multicast  string
+	bytes      int
+	packets    int
+	errs       int
+	drop       int
+	fifo       int
+	frame      int
+	compressed int
+	multicast  int
 }
 
 func main() {
 	getProcNetDev()
 	fmt.Println(netInfo)
+}
+
+func Show() {
+	for {
+		netrefresh()
+		for _, n := range netDevices {
+			fmt.Printf("%s: %dKb/s\n", n.face, n.Receive.bytes/1024)
+		}
+		time.Sleep(time.Duration(1) * time.Second)
+	}
+}
+
+func netrefresh() {
+	netDevices = netDevices[:0]
+	getProcNetDev()
+	getLine()
+	getNetDevices()
 }
 
 func getProcNetDev() {
@@ -49,5 +68,33 @@ func getProcNetDev() {
 
 func getLine() {
 	netLin = strings.Split(netInfo, "\n")
-	netLin = netLin[2:]
+	netLin = netLin[2 : len(netLin)-1]
+}
+
+func getNetDevices() {
+	for i := 0; i < len(netLin); i++ {
+		ts := strings.Fields(netLin[i])
+		var rn NetInfo
+		rn.bytes, _ = strconv.Atoi(ts[1])
+		rn.packets, _ = strconv.Atoi(ts[2])
+		rn.errs, _ = strconv.Atoi(ts[3])
+		rn.drop, _ = strconv.Atoi(ts[4])
+		rn.fifo, _ = strconv.Atoi(ts[5])
+		rn.frame, _ = strconv.Atoi(ts[6])
+		rn.compressed, _ = strconv.Atoi(ts[7])
+		rn.multicast, _ = strconv.Atoi(ts[8])
+		var tn NetInfo
+		tn.bytes, _ = strconv.Atoi(ts[9])
+		tn.packets, _ = strconv.Atoi(ts[10])
+		tn.errs, _ = strconv.Atoi(ts[11])
+		tn.drop, _ = strconv.Atoi(ts[12])
+		tn.fifo, _ = strconv.Atoi(ts[13])
+		tn.frame, _ = strconv.Atoi(ts[14])
+		tn.compressed, _ = strconv.Atoi(ts[15])
+		tn.multicast, _ = strconv.Atoi(ts[16])
+
+		dv := NetDevice{strings.Trim(ts[0], ":"), rn, tn}
+
+		netDevices = append(netDevices, dv)
+	}
 }
--- main/main_test.go
+++ main/main_test.go
@@ -8,5 +8,18 @@ import (
 func TestGetline(t *testing.T) {
 	getProcNetDev()
 	getLine()
-	fmt.Println(netLin[0])
+	//tmp := strings.Fields(netLin[0])
+	//fmt.Println(tmp)
+	fmt.Println(len(netLin))
+}
+
+func TestGetdevices(t *testing.T) {
+	getProcNetDev()
+	getLine()
+	getNetDevices()
+	fmt.Println(netDevices)
+}
+
+func TestShow(t *testing.T) {
+	Show()
 }
